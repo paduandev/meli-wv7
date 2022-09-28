0473dc71b98c1e24954403462fdc4c5424e927d5
EmersonPaduan <emerson.paduan@gmail.com>
2022-09-28 15:06:37 -0300

    add conta especial

--- aula05/src/modelo/ContaEspecial.java
+++ aula05/src/modelo/ContaEspecial.java
@@ -2,10 +2,12 @@ package modelo;
 
 import exception.InvalidNumberException;
 
-public class ContaCorrente extends Conta {
+public class ContaEspecial extends Conta {
+    private double limite;
 
-    public ContaCorrente(int numero, Cliente cliente) {
+    public ContaEspecial(int numero, Cliente cliente, double limite) {
         super(numero, cliente);
+        this.limite = limite;
     }
 
     @Override
@@ -17,11 +19,11 @@ public class ContaCorrente extends Conta {
     }
 
     private boolean saldoInsuficiente(double valor) {
-        return getSaldo() < valor;
+        return getSaldo() + limite < valor;
     }
 
     @Override
     public String toString() {
-        return "Conta Corrente: " + super.toString();
+        return "Conta Especial: " + super.toString();
     }
 }
